b3dc723a5f240c7d5aaef0bb86f2c355d81e71a7
dsh-mobile <local@dsh>
2026-09-12 12:24:15 +0800

    fix: revert MainActivity to standard Capacitor template, remove loadPlugin call

--- android/app/src/main/java/com/calista/southeastaurelia/MainActivity.java
+++ android/app/src/main/java/com/calista/southeastaurelia/MainActivity.java
@@ -1,14 +1,5 @@
 package com.calista.southeastaurelia;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Register custom Capacitor plugins here
-        this.loadPlugin(MyPlugin.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
